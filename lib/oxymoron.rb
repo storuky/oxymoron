de06c41a4ee58bcb843a25d0155aac28edc91407
Paul Kononenko <storuky@gmail.com>
2018-05-29 19:37:08 +0300

    skip state

--- lib/oxymoron.rb
+++ lib/oxymoron.rb
@@ -16,9 +16,11 @@ module Oxymoron
       @app_routes_by_controller = @app_routes.select{|route| ['new', 'edit', 'show', 'index'].exclude?(route.defaults[:action])}.group_by{|route| route.defaults[:controller]}.delete_if {|k,v| k.nil?}
       
       @app_routes.each do |route|
-        set_routes route
-        set_states(route) unless route.defaults[:skip_state]
-        set_resources route
+        unless route.defaults[:skip_state]
+          set_routes(route)
+          set_states(route)
+          set_resources(route)
+        end
       end
     end
 
